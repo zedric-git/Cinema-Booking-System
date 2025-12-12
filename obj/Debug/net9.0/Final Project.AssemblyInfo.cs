93b0060cd47c90cb1c361d888072772846c36d11
zedric-git <camilotes.zedric@gmail.com>
2025-12-12 23:36:25 +0800

    (1) Updated Website

--- obj/Debug/net9.0/Final Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Final Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd681b5b232f09899d0cae9520a367e01ea1433")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
